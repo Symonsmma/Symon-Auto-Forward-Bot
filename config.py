c42d865b61c6c6ee0638b9b9fc677aba4718763e
Symonsmma <symonsmma@gmail.com>
2024-11-21 17:51:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-      API_ID = int(getenv("API_ID", "20389440"))
+      API_HASH = getenv("API_HASH", "06c23ba5a896feb881b048f56f714edc")
+      API_ID = int(getenv("API_ID", "22820722"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001722984461:-1001623633000").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "7793548536:AAH1QMNv1IjERK28T8zeUDieldD-l3hvUYI")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002417706539:-1002314753184").replace("\n", " ").split(' '))
 
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @symonads
+# Subscribe YouTube Channel For Amazing Bot @symonads
+# Ask Doubt on telegram @symonads
